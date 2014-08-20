6e8b00be9bfda50e4f5f8072c2ee2c02d589e9db
BalestraPatrick <infobalestra@gmail.com>
2014-08-20 19:38:54 +0200

    Fixed a few mistakes introduced with the previous PR

--- Passcode Window/Passcode.c
+++ Passcode Window/Passcode.c
@@ -1,7 +1,7 @@
 /*
-    Developed by: Patrick Balestra
-    Twitter: @BalestraPatrick
-*/
+ Developed by: Patrick Balestra
+ Twitter: @BalestraPatrick
+ */
 
 #include <pebble.h>
 #include "Home.h"
@@ -25,30 +25,21 @@ static char saved_passcode[5];
 
 static int wrote_passcode_int = 0;
 
-//Keep track of how many number layers we've initialized
-//to insert that number as the text on said layer.
-static int layer_init = 0;
-
 // Functions
 
-static TextLayer* number_layer_init(GRect location)
-{
-    TextLayer *layer;
-    static char buffer[] = "0";
-	text_layer_create(location);
+static TextLayer* number_layer_init(GRect location, char *index) {
+    TextLayer *layer = text_layer_create(location);
     text_layer_set_background_color(layer, GColorWhite);
     text_layer_set_text_color(layer, GColorBlack);
     text_layer_set_font(layer, fonts_get_system_font(FONT_KEY_GOTHIC_14));
+    text_layer_set_text(layer, index);
     text_layer_set_text_alignment(layer, GTextAlignmentCenter);
-    snprintf(buffer, sizeof(buffer), "%d", layer_init);
-    text_layer_set_text(layer, buffer);
-    layer_init++;
-	return layer;
+    return layer;
 }
 
 static void update_bar() {
     inverter_layer_destroy(highlighter);
-    int height = 15*selected_number;
+    int height = 15 * selected_number;
     highlighter = inverter_layer_create(GRect(124, height, 20, 15));
     layer_add_child(window_get_root_layer(window), inverter_layer_get_layer(highlighter));
 }
@@ -65,9 +56,9 @@ static void wrong_passcode() {
     bitmap_layer_set_bitmap(second_layer, circle_icon);
     bitmap_layer_set_bitmap(third_layer, circle_icon);
     bitmap_layer_set_bitmap(fourth_layer, circle_icon);
-
+    
     text_layer_set_text(status_text_layer, "Wrong passcode.\nTry again.");
-
+    
 }
 
 static void check_passcode() {
@@ -128,7 +119,6 @@ static void select_single_click_handler(ClickRecognizerRef recognizer, void *con
             check_passcode();
             break;
     }
-    }
 }
 
 static void config_provider(Window *window) {
@@ -138,54 +128,54 @@ static void config_provider(Window *window) {
 }
 
 static void window_load(Window *window) {
-
+    
     persist_read_string(1, saved_passcode, 50);
-
+    
     Layer *window_layer = window_get_root_layer(window);
-
-    number_0 = number_layer_init(GRect(124, 0, 20, 15));
+    
+    number_0 = number_layer_init(GRect(124, 0, 20, 15), "0");
     layer_add_child(window_layer, text_layer_get_layer(number_0));
     
-    number_1 = number_layer_init(GRect(124, 15, 20, 15));
+    number_1 = number_layer_init(GRect(124, 15, 20, 15), "1");
     layer_add_child(window_layer, text_layer_get_layer(number_1));
-
-    number_2 = number_layer_init(GRect(124, 30, 20, 15));
+    
+    number_2 = number_layer_init(GRect(124, 30, 20, 15), "2");
     layer_add_child(window_layer, text_layer_get_layer(number_2));
-
-    number_3 = number_layer_init(GRect(124, 45, 20, 15));
+    
+    number_3 = number_layer_init(GRect(124, 45, 20, 15), "3");
     layer_add_child(window_layer, text_layer_get_layer(number_3));
-
-    number_4 = number_layer_init(GRect(124, 60, 20, 15));
+    
+    number_4 = number_layer_init(GRect(124, 60, 20, 15), "4");
     layer_add_child(window_layer, text_layer_get_layer(number_4));
-
-    number_5 = number_layer_init(GRect(124, 75, 20, 15));
+    
+    number_5 = number_layer_init(GRect(124, 75, 20, 15), "5");
     layer_add_child(window_layer, text_layer_get_layer(number_5));
-
-    number_6 = number_layer_init(GRect(124, 90, 20, 15));
+    
+    number_6 = number_layer_init(GRect(124, 90, 20, 15), "6");
     layer_add_child(window_layer, text_layer_get_layer(number_6));
-
-    number_7 = number_layer_init(GRect(124, 105, 20, 15));
+    
+    number_7 = number_layer_init(GRect(124, 105, 20, 15), "7");
     layer_add_child(window_layer, text_layer_get_layer(number_7));
-
-    number_8 = number_layer_init(GRect(124, 120, 20, 15));
+    
+    number_8 = number_layer_init(GRect(124, 120, 20, 15), "8");
     layer_add_child(window_layer, text_layer_get_layer(number_8));
-
-    number_9 = number_layer_init(GRect(124, 135, 20, 17));
+    
+    number_9 = number_layer_init(GRect(124, 135, 20, 17), "9");
     layer_add_child(window_layer, text_layer_get_layer(number_9));
-
+    
     status_text_layer = text_layer_create(GRect(0, 80, 124, 40));
     text_layer_set_text_color(status_text_layer, GColorBlack);
     text_layer_set_font(status_text_layer, fonts_get_system_font(FONT_KEY_GOTHIC_18));
     text_layer_set_text_alignment(status_text_layer, GTextAlignmentCenter);
     layer_add_child(window_get_root_layer(window), text_layer_get_layer(status_text_layer));
-
+    
     selected_number = 0;
     cursor_position = 0;
     
     update_bar();
-  
+    
     window_set_click_config_provider(window, (ClickConfigProvider) config_provider);
-
+    
     circle_icon = gbitmap_create_with_resource(RESOURCE_ID_CIRCLE);
     circle_filled_icon = gbitmap_create_with_resource(RESOURCE_ID_CIRCLE_FILLED);
     
@@ -209,9 +199,9 @@ static void window_load(Window *window) {
     bitmap_layer_set_alignment(first_layer, GAlignCenter);
     layer_add_child(window_get_root_layer(window), bitmap_layer_get_layer(fourth_layer));
     bitmap_layer_set_bitmap(fourth_layer, circle_icon);
-
+    
     line = gbitmap_create_with_resource(RESOURCE_ID_LINE);
-
+    
     line_layer = bitmap_layer_create(GRect(122, 0, 2, 168));
     bitmap_layer_set_alignment(line_layer, GAlignCenter);
     layer_add_child(window_get_root_layer(window), bitmap_layer_get_layer(line_layer));
